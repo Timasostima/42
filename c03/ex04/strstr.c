4d2ce9ba8a7710167caea9830a85c6cb570cac45
Tymur Kulivar Shymanskyi <72166965+Timasostima@users.noreply.github.com>
2024-07-18 19:04:03 +0200

    A lot of errors

---
+++ c03/ex04/strstr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strstr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkulivar <tkulivar@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 12:12:13 by tkulivar          #+#    #+#             */
+/*   Updated: 2024/07/18 17:22:25 by tkulivar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	sub_i;
+
+	i = 0;
+	if (to_find[i] == '\0')
+		return (&str[i]);
+	while (str[i] != '\0')
+	{
+		sub_i = 0;
+		while (to_find[sub_i] == str[i + sub_i] && str[i + sub_i] != '\0')
+		{
+			if (to_find[sub_i + 1] == '\0')
+				return (&str[i]);
+			sub_i++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+// int	main(void)
+// {
+// 	char	*str;
+// 	char	*sub_str;
+// 	char	*ss_pos;
+// 	str = "Hello World!";
+// 	sub_str = "World";
+// 	ss_pos = ft_strstr(str, sub_str);
+// 	return(0);	
+// }
---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkulivar <tkulivar@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 11:51:01 by tkulivar          #+#    #+#             */
+/*   Updated: 2024/07/18 17:09:39 by tkulivar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size < dest_len)
+		return (src_len + size);
+	while (src[i] != '\0' && i < size - dest_len - 1)
+	{
+		dest[i + dest_len] = src[i];
+		i++;
+	}
+	dest[i + dest_len] = '\0';
+	return (dest_len + src_len);
+}
+
+// #include <bsd/string.h>
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	*src = "Hola";
+// 	char	dest[20] = "Tymur";
+// 	char	dest1[20] = "Tymur";
+// 	int res = ft_strlcat(dest, src, 7);
+// 	int res1 = strlcat(dest1, src, 7);
+// 	printf("%i\n", res);
+// 	printf("%i\n", res1);
+// 	printf("%s\n", dest);
+// 	printf("%s\n", dest1);
+// 	return (0);	
+// }
