671666f426a9b3ea607ec2130e87b5948f5718e2
Tymur Kulivar Shymanskyi <contact@tymurkulivar.dev>
2025-08-02 19:22:55 +0200

    remove useless files

--- cursus/printf/ft_printf.c
+++ cursus/printf/ft_printf.c
@@ -1,140 +1,5 @@
 #include "ft_printf.h"
 
-// Helper function implementations
-int ft_print_char(char c)
-{
-    ft_putchar_fd(c, 1);
-    return 1;
-}
-
-int ft_print_str(char *s)
-{
-    int len;
-
-    if (!s)
-        s = "(null)";
-    len = ft_strlen(s);
-    ft_putstr_fd(s, 1);
-    return len;
-}
-
-int ft_print_ptr(void *ptr)
-{
-    unsigned long addr = (unsigned long)ptr;
-    int printed = 0;
-    char hex_digits[] = "0123456789abcdef";
-    char buffer[20];
-    int i = 0;
-
-    if (!ptr)
-    {
-        ft_putstr_fd("(nil)", 1);
-        return 5;
-    }
-    
-    ft_putstr_fd("0x", 1);
-    printed += 2;
-    
-    if (addr == 0)
-    {
-        ft_putchar_fd('0', 1);
-        return printed + 1;
-    }
-    
-    while (addr > 0)
-    {
-        buffer[i++] = hex_digits[addr % 16];
-        addr /= 16;
-    }
-    
-    while (--i >= 0)
-    {
-        ft_putchar_fd(buffer[i], 1);
-        printed++;
-    }
-    
-    return printed;
-}
-
-int ft_print_int(int n)
-{
-    char *str = ft_itoa(n);
-    int len;
-
-    if (!str)
-        return 0;
-    len = ft_strlen(str);
-    ft_putstr_fd(str, 1);
-    free(str);
-    return len;
-}
-
-int ft_print_unsigned(unsigned int n)
-{
-    char buffer[12];
-    int i = 0;
-    int printed = 0;
-
-    if (n == 0)
-    {
-        ft_putchar_fd('0', 1);
-        return 1;
-    }
-    
-    while (n > 0)
-    {
-        buffer[i++] = '0' + (n % 10);
-        n /= 10;
-    }
-    
-    while (--i >= 0)
-    {
-        ft_putchar_fd(buffer[i], 1);
-        printed++;
-    }
-    
-    return printed;
-}
-
-int ft_print_hex(unsigned int n, int uppercase)
-{
-    char *hex_digits;
-    char buffer[10];
-    int i = 0;
-    int printed = 0;
-
-    if (uppercase)
-        hex_digits = "0123456789ABCDEF";
-    else
-        hex_digits = "0123456789abcdef";
-    
-    if (n == 0)
-    {
-        ft_putchar_fd('0', 1);
-        return 1;
-    }
-    
-    while (n > 0)
-    {
-        buffer[i++] = hex_digits[n % 16];
-        n /= 16;
-    }
-    
-    while (--i >= 0)
-    {
-        ft_putchar_fd(buffer[i], 1);
-        printed++;
-    }
-    
-    return printed;
-}
-
-int ft_print_percent(void)
-{
-    ft_putchar_fd('%', 1);
-    return 1;
-}
-
 int ft_handle_format(char c, va_list args)
 {
     if (c == 'c')
@@ -156,19 +21,19 @@ int ft_handle_format(char c, va_list args)
     return 0;
 }
 
-int ft_printf(const char *format, ...)
+int ft_printf(const char *to_format, ...)
 {
     va_list args;
     int i = 0, printed = 0;
 
-    va_start(args, format);
-    while (format[i])
+    va_start(args, to_format);
+    while (to_format[i])
     {
-        if (format[i] == '%' && format[i + 1])
-            printed += ft_handle_format(format[++i], args);
+        if (to_format[i] == '%' && to_format[i + 1])
+            printed += ft_handle_format(to_format[++i], args);
         else
         {
-            ft_putchar_fd(format[i], 1);
+            ft_putchar_fd(to_format[i], 1);
             printed++;
         }
         i++;
--- cursus/printf/ft_printf.h
+++ cursus/printf/ft_printf.h
@@ -8,13 +8,4 @@
 
 int ft_printf(const char *format, ...);
 
-// Format handlers
-int ft_print_char(char c);
-int ft_print_str(char *s);
-int ft_print_ptr(void *ptr);
-int ft_print_int(int n);
-int ft_print_unsigned(unsigned int n);
-int ft_print_hex(unsigned int n, int uppercase);
-int ft_print_percent(void);
-
 #endif
